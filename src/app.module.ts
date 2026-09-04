ee250eacb0ea38fce85ee8342ecfc79aea0f235f
Bohdan Kutsyk <mkkbodya@gmail.com>
2026-09-04 22:07:03 +0300

    remove password log on database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,9 +18,6 @@ import { readFile } from 'node:fs/promises';
         const DB_PASSWORD_FILE = configService.get(
           'POSTGRES_PASSWORD_FILE',
         ) as string;
-        const DB_PASSWORD = (await readFile(DB_PASSWORD_FILE, 'utf-8')).trim();
-
-        console.log('password:', DB_PASSWORD.slice(0, 5) + '***');
 
         return {
           type: 'postgres',
