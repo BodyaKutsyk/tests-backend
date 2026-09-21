01f5f7ab8550afc16ba74bd94e616e00ba447f37
Bohdan Kutsyk <mkkbodya@gmail.com>
2026-09-21 20:09:25 +0300

    add skip vault and migrate script

--- infisical.sh
+++ infisical.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+if [[ -f .env ]]; then
+  set -a
+  source .env
+  set +a
+fi
+
+if [ "${SKIP_VAULT:-0}" = "1" ]; then exec "$@"; fi
+
 if [[ -f .env.infisical ]]; then
   set -a
   source .env.infisical
